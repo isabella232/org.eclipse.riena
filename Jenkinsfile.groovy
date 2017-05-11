cd2d648e23f5c1e1d0ae8677147581103972b805
Stefan Geiger <Stefan.Geiger@compeople.de>
2017-05-11 11:33:15 +0200

    Optimized exception handling to forward them to Jenkins.

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -25,22 +25,18 @@ node('windows10 && x86 && jdk8') {
 			// Run Maven build with pre-configured Maven installation named 'M3'.
 			// Note: This also runs the JUnit plugin-tests via tycho-surefire-plugin.
 			withMaven(maven: 'M3') {
-				try{
+				try {
 					// fail at end so we finish the build and than fail, ignore testfailures(well collect the test failures later in the reporting pahse)
 					bat "mvn -fae clean integration-test -DforceContextQualifier=HEAD_" + getBuildTimestamp() + " -Dmaven.test.failure.ignore=true"
-					} catch(org.jenkinsci.plugins.scriptsecurity.sandbox.RejectedAccessException e){
-		        print "catched exception "+e;
-		        currentBuild.result = 'FAILURE'
-		        throw e;
-		      }
-		      catch(err){
-		        String error = "${err}"
-		        //send mail with the catched error
-		        sendErrorMail(error)
-		        // if the build encounters an failure mark the build as failed
-		        currentBuild.result = 'FAILURE'
-		      }
-		     }
+				} catch (err) {
+					String error = "${err}"
+					//send mail with the catched error
+					sendErrorMail(error)
+					// if the build encounters an failure mark the build as failed
+					currentBuild.result = 'FAILURE'
+					throw err;
+				}
+
 			}
 
 		}
@@ -66,7 +62,7 @@ node('windows10 && x86 && jdk8') {
 			//archiveArtifacts artifacts: 'org.eclipse.riena.tests.optional/target/surefire-reports/*', fingerprint: true
 		}
 	}
-
+}
 
 def String getBuildTimestamp() {
 	def buildTimestampMillis = currentBuild.getStartTimeInMillis()
